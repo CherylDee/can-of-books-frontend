9ae7a14208a9e177f526b507e7e76eff12cf31e5
Cheryl Dee <cdmccalop@gmail.com>
2023-03-15 11:24:23 -0700

    added images

--- src/About.js
+++ src/About.js
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import coffeeImg from './images/coffee.png';
+import hulkImg from './images/hulk.jpg';
 
 class Profile extends Component {
 
@@ -6,8 +8,9 @@ class Profile extends Component {
     /* TODO: render information about the developers */
     return (
       <>
-      <img src="./images/Coffee3.png" alt='this is Coffee from Cowboy Bebop'></img>
+      <img src={ coffeeImg } alt='this is Coffee from Cowboy Bebop'></img>
       <div className='about'><p>Hi, I'm Cheryl, an older student who has been studying front-end development since 2009, though you'll never know because I still act and feel like a neophyte. </p>
+      <img src={ hulkImg } alt="this is Hulk Hogan - Mo's alter ego"></img>
       <p>My backend partner we call Mo, is an Audio Engineer from Everett WA, aspiring to become the next power-play billionaire.</p></div>
         
       </>

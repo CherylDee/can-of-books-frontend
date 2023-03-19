f08143b6ad7547a93838bb1b573ea3e852e7a804
Mohammed Abubakar <mohammedfahim363@yahoo.com>
2023-03-19 15:23:37 -0700

    editz

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -13,9 +13,13 @@ class BestBooks extends React.Component {
     this.state = {
       books: [],
       showModal: false,
+      selectedBook: {}
     }
   }
-
+  handleBook = () => {
+    this.props.handleOpenModal();
+  }
+  
   handleClose = () => {
     this.setState({
       showModal: false,
@@ -26,7 +30,7 @@ class BestBooks extends React.Component {
   handleShow = () => {
     this.setState ({
       showModal: true,
-
+      selectedBook: book,
     })
   }
 
@@ -34,6 +38,7 @@ class BestBooks extends React.Component {
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
   getBooks = async () => {
     try {
+     //let url = `${process.env.REACT_APP_SERVER}/Book/${id}`;
       let bookResults = await axios.get(`${process.env.REACT_APP_SERVER}/books`);
       this.setState({
         books: bookResults.data
@@ -52,6 +57,7 @@ class BestBooks extends React.Component {
   
     deleteBook = async (id) => {
       try {
+        let url = `${process.env.REACT_APP_SERVER}/Book/${id}`;
         let updateBooks= await axios.delete(`${process.env.REACT_APP_SERVER}/books/${id}`);
         this.state.books.filter(book => book._id !== id);
         this.setState({
@@ -80,8 +86,27 @@ class BestBooks extends React.Component {
 
   }
 
+  updateBooks = async (booktoUpdate) => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/Books/${booktoUpdate._id}` 
+      let updatedBookFromDB = await axios.put(url, booktoUpdate);  
+      let updatedBook = this.state.books.map((existingBook) => {
+        return existingBook._id === booktoUpdate._id
+          ? updatedBook.data
+          : book;
+      });
+      this.setState({
+        cats: updatedBook
+      })
+      console.log(updatedBook)
+    } catch (error) {
+      console.log('we have an error:', error.response.data)
+    }
+  }
+
     postBook = async (bookObj) => {
       try {
+        let url = `${process.env.REACT_APP_SERVER}/Book/${id}`;
         let createdBook = await axios.post(`${process.env.REACT_APP_SERVER}/books/${bookObj}`);
         this.setState({
           books: [...this.state.books, createdBook.data]
@@ -106,7 +131,8 @@ class BestBooks extends React.Component {
 
     /* TODO: render all the books in a Carousel */
     let booksArray = this.state.books.map((book, _id) => {
-      return (  <Carousel.Item key={_id}>
+      return (  
+      <Carousel.Item key={book._id}>
                   {/* <Button variant="success" onClick={() => this.handleStartUpdating(book)}>Update Book</Button> */}
                   <img src={bookImg} alt="books about getting tryin' to get rich" />
               
@@ -115,6 +141,8 @@ class BestBooks extends React.Component {
                     <h4>{book.title}</h4>
                     <h5>{book.status}</h5>
                     <p>{book.description}</p>
+                    <Button onClick={() => this.deleteBook(book._id)}>Delete Book</Button>
+                    <Button onClick={() => { this.handleUpdateModal(book) }}>Update</Button>
                   </Carousel.Caption>
                 </Carousel.Item>
       )
@@ -125,6 +153,7 @@ class BestBooks extends React.Component {
         <h2>The Get Rich Learning Library</h2>
 
         {this.state.books.length ? (
+    
           <Carousel variant='dark'>
             {booksArray}
           </Carousel>
